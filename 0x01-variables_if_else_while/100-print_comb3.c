3dde09457d61f7c681f852414322e98bc4323efe
oluwaseun <ibuemmanuel60@gmail.com>
2022-10-14 17:32:51 -0700

    any

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,8 +15,15 @@ int main(void)
 			if (m > n)
 			{
 				putchar(n);
-				putchar(m)	
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
-}	
+	putchar('\n');
+	return (0);
+}
